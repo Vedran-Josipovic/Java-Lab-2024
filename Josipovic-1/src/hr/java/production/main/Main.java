11cce9aa0cd618f89c91af60661828c3240420c3
Vedran-Josipovic <vedranjosipovic.posao@gmail.com>
2023-10-11 13:33:17 +0200

    Added initial classes and started Main

---
+++ Josipovic-1/src/hr/java/production/main/Main.java
@@ -0,0 +1,24 @@
+package hr.java.production.main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    private static final Integer NUM_CATEGORIES = 3;
+    private static final Integer NUM_ITEMS = 5;
+    private static final Integer NUM_FACTORIES = 2;
+    private static final Integer NUM_STORES = 2;
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+
+        //File file = new File("Josipovic-1/src/hr/java/production/files/currentInput");
+        //Scanner scanner = new Scanner(file);
+
+
+    }
+
+
+}
